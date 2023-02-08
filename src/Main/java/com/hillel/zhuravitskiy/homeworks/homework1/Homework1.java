c2f946ad69291071bf3c67f02f583344f1882e21
Oleksii Zhuravytskyi <alexzhuravytskyi@gmail.com>
2023-02-08 20:43:42 +0200

    init commit

---
+++ src/Main/java/com/hillel/zhuravitskiy/homeworks/homework1/Homework1.java
@@ -0,0 +1,9 @@
+package com.hillel.zhuravitskiy.homeworks.homework1;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello word!");
+        System.out.print("Hello word!");
+    }
+}
