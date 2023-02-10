e4e14144ab148a7e48b73128f225db6b7d869463
Oleksii Zhuravytskyi <alexzhuravytskyi@gmail.com>
2023-02-10 13:27:16 +0200

    added all files

---
+++ src/Main/java/com/hillel/zhuravitskiy/homeworks/homework2/Main.java
@@ -0,0 +1,22 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.hillel.zhuravitskiy.homeworks.homework2;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        int c = 9;
+        int volume = a * b * c;
+        System.out.println(volume);
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        int length = 4 * (a + b + c);
+        System.out.println(length);
+    }
+}
---
+++ src/Main/java/com/hillel/zhuravitskiy/homeworks/homework3/Main.java
@@ -0,0 +1,27 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.hillel.zhuravitskiy.homeworks.homework3;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int a = 46;
+        char g = 176;
+        int b = 42;
+        char m = 8217;
+        float c = 18.486F;
+        char n = 'N';
+        int d = 30;
+        int f = 47;
+        float k = 52.5804F;
+        char e = 'E';
+        System.out.println("Переможне coordinates:");
+        System.out.println("DNS Lat: " + a + g + " " + b + m + " " + c + m + m + " " + n + " ");
+        System.out.println("DMS Long: " + d + g + " " + f + m + " " + k + m + m + " " + e + " ");
+    }
+}
