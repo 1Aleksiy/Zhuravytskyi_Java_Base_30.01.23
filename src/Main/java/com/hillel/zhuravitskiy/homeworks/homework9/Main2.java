f1b956d7790e830b0c839b12061f64565db2ffe6
Oleksii Zhuravytskyi <alexzhuravytskyi@gmail.com>
2023-03-12 19:42:27 +0200

    homework11

--- src/Main/java/com/hillel/zhuravitskiy/homeworks/homework9/Main2.java
+++ src/Main/java/com/hillel/zhuravitskiy/homeworks/homework9/Main2.java
@@ -1,6 +1,4 @@
 package com.hillel.zhuravitskiy.homeworks.homework9;
-
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.IntStream;
@@ -21,8 +19,8 @@ public class Main2 {
             }
         }
         System.out.println("Введіть кількість стовбців");
-        int m = 0;
-        while (true) {
+             int m = 0;
+                while (true) {
             if (scanner.hasNextInt()) {
                 m = scanner.nextInt();
                 System.out.println(m);
@@ -41,44 +39,27 @@ public class Main2 {
         IntStream.range(0, rows)
                 .forEach(row -> IntStream.range(0, columns)
                         .forEach(column -> array1[row][column] = random.nextInt(50)));
-        System.out.println(Arrays.deepToString(array1) + " ");
-
-
+        System.out.println("Перший масив :");
+        printArray(array1);
         System.out.println();
-        for (n = 0; n <array2.length; n++) {
-            for (m = n + 1; m < array2[n].length; m++) {
 
-                             int temp = array1[n][m];
-                array1[n][m] = array2[m][n];
-                             array2[m][n] = temp;
+
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = array1[j][i];
             }
         }
-        System.out.println();
-        for (n = 0; n <array1.length; n++) {
-            for (m = n + 1; m < array1[n].length; m++) {
+        System.out.println("Другий масив :");
+        printArray(array2);
 
-        }
-            System.out.print(Arrays.deepToString(array2) + " ");
+        scanner.close();
+    }
 
-                }
-
+    static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
             }
-
         }
-//        System.out.println();
-//        for (n = 0; n < array1.length; n++) {
-//            for (m = n + 1; m < array1.length; m++) {
-
-
-//               IntStream.range(0, 44);
-//                      .filter(m ->m % 10 < 5 )
-//                     .filter(n -> n/ 10 > n % 10)
-//                     .forEach(n ->{
-//                         int temp = array1[n/10][m%10] = array2[m%10][n/10];
-//                          array2[m%10][n/10] = temp;
-//                      });
-
-
-
-
-
+    }
+}
